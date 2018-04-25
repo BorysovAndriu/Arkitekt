e7247311353add4912f3d5351fca663289e04b04
BorysovAndriu <andriu.test.opst.1@gmail.com>
2018-04-25 22:26:24 +0300

    CodeStyle2

--- sandbox/src/main/java/io/arkitekt/test/MyFirstProgram.java
+++ sandbox/src/main/java/io/arkitekt/test/MyFirstProgram.java
@@ -1,7 +1,18 @@
 package io.arkitekt.test;
 
 public class MyFirstProgram {
-	public static void main (String[] args){
-		System.out.println("Hello World");
-	} 
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+
+    System.out.println(2+2);
+    System.out.println(2-2);
+    System.out.println(2/2);
+    System.out.println(2*2);
+
+    System.out.println(1.0/2);
+
+    System.out.println("2"+2);
+    System.out.println((2+2)*2);
+
+  }
 }
