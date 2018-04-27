9754e40f352e5026e7062e66eb3f79c01e4404ae
BorysovAndriu <andriu.test.opst.1@gmail.com>
2018-04-28 01:03:47 +0300

    StartTestforSignIn

--- arkitekt/src/test/java/io/arkitekt/SignIn.java
+++ arkitekt/src/test/java/io/arkitekt/SignIn.java
@@ -2,6 +2,7 @@ package io.arkitekt;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.*;
 
@@ -14,30 +15,34 @@ public class SignIn {
   public void setUp() throws Exception {
     driver = new FirefoxDriver();
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    driver.get("http://staging.arkitekt.io./");
+    login("andriu.test.post.1@gmail.com", "1111111111");
+  }
+
+  private void login(String email, String password) {
+    driver.findElement(By.linkText("Sign in")).click();
+    driver.findElement(By.id("user_email")).clear();
+    driver.findElement(By.id("user_email")).sendKeys(email);
+    driver.findElement(By.id("user_password")).click();
+    driver.findElement(By.id("user_password")).clear();
+    driver.findElement(By.id("user_password")).sendKeys(password);
+    driver.findElement(By.name("commit")).click();
   }
 
   @Test
-  public void testSignInLogout() throws Exception {
-    driver.get("http://staging.arkitekt.io./");
-    driver.findElement(By.linkText("Sign in")).click();
-    driver.findElement(By.id("user_email")).clear();
-    driver.findElement(By.id("user_email")).sendKeys("andriu.test.post.1@gmail.com");
-    driver.findElement(By.id("user_password")).click();
-    driver.findElement(By.id("user_password")).clear();
-    driver.findElement(By.id("user_password")).sendKeys("1111111111");
-    driver.findElement(By.name("commit")).click();
+  public void testSignIn() throws Exception {
+    initListSite();
+   }
+
+
+  public void initListSite() {
     driver.findElement(By.id("choose_template")).click();
-    driver.findElement(By.name("commit")).click();
-    driver.findElement(By.xpath("//div[@id='content']/div/div/a")).click();
   }
 
   @AfterMethod
   public void tearDown() throws Exception {
     driver.quit();
   }
-
-
-
 
 
 }
