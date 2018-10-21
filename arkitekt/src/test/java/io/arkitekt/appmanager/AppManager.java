0779bb66b93dc45c612f0a09c5fd4627e1cca5d7
BorysovAndriu <36803019+BorysovAndriu@users.noreply.github.com>
2018-10-21 12:04:39 +0300

    Реалізовано відалений запуск браузера

--- arkitekt/src/test/java/io/arkitekt/appmanager/AppManager.java
+++ arkitekt/src/test/java/io/arkitekt/appmanager/AppManager.java
@@ -6,11 +6,19 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class AppManager {
 
+    private final Properties properties;
     public WebDriver driver;
     Actions builder;
 
@@ -23,25 +31,30 @@ public class AppManager {
 
     public AppManager(String browser) {
         this.browser = browser;
+        properties = new Properties();
     }
 
-    public AppManager() {
-    }
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
-    public void init() throws InterruptedException {
-
-        if (browser.equals(BrowserType.FIREFOX)) {
-            driver = new FirefoxDriver();
-        } else if (browser.equals(BrowserType.CHROME)) {
-            driver = new ChromeDriver();
-        } else if (browser.equals(BrowserType.EDGE)) {
-            driver = new EdgeDriver();
+        if ("".equals(properties.getProperty("selenium.server"))) {
+            if (browser.equals(BrowserType.FIREFOX)) {
+                driver = new FirefoxDriver();
+            } else if (browser.equals(BrowserType.CHROME)) {
+                driver = new ChromeDriver();
+            } else if (browser.equals(BrowserType.EDGE)) {
+                driver = new EdgeDriver();
+            }
+        } else {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName(browser);
+            driver = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
         }
 
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-
+        driver.get(properties.getProperty("web.baseUrl"));
         driver.manage().window().maximize();
-
         builder = new Actions(driver);
 
         loginHelper = new io.arkitekt.appManager.LoginHelper(driver);
--- arkitekt/src/test/java/io/arkitekt/testsRegistration/TestBase.java
+++ arkitekt/src/test/java/io/arkitekt/testsRegistration/TestBase.java
@@ -7,12 +7,12 @@ import org.testng.annotations.BeforeMethod;
 
 public class TestBase {
 
-    protected static final AppManager app = new AppManager(BrowserType.CHROME);
+    protected static final AppManager app = new AppManager(System.getProperty("browser", BrowserType.CHROME));
 
     @BeforeMethod
     public void setUpp() throws Exception {
         app.init();
-        app.getNavHelperLeftPanel().openSite("http://staging.arkitekt.io/");
+        //app.getNavHelperLeftPanel().openSite("http://staging.arkitekt.io/");
         app.getLoginHelper().login("andriu2018.10.04@gmail.com", "1111111111");
     }
 
