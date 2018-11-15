31983316a2cea6b27b4e76a7c57354ee575747df
BorysovAndriu <36803019+BorysovAndriu@users.noreply.github.com>
2018-11-15 18:08:28 +0200

    Fix

--- arkitekt/src/test/java/io/arkitekt/testsWidgets/TestBaseWidgets.java
+++ arkitekt/src/test/java/io/arkitekt/testsWidgets/TestBaseWidgets.java
@@ -2,14 +2,10 @@ package io.arkitekt.testsWidgets;
 
 import io.arkitekt.appManager.operationWidgets.AppManagerWidgets;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-
-import java.lang.reflect.Method;
 
 public class TestBaseWidgets {
 
@@ -21,7 +17,7 @@ public class TestBaseWidgets {
         widgets.getWidgetsHelper().openSite("http://staging.arkitekt.io/");
         widgets.driver.manage().addCookie(widgets.getPagesHelper().readeCookies());
         widgets.getPagesHelper().click(By.xpath("//a[@class='btn btn-link']"));
-        //widgets.getPagesHelper().click(By.cssSelector("#pages"));
+        widgets.getPagesHelper().click(By.cssSelector("#pages"));
 
         /**
          widgets.getWidgetsHelper().initListSite(true);
@@ -32,36 +28,18 @@ public class TestBaseWidgets {
          widgets.getWidgetsHelper().openFirstSite();
          }*/
     }
-
+/*
     @BeforeMethod
-    public void howTest(Method method) throws InterruptedException {
-        String nameTest = method.getName();
-        if (nameTest == "addPage") {
+    public void openSettingPage() throws InterruptedException {
+        //String nameTest = method.getName() //Method method;
+        if (widgets.getPagesHelper().isElementDesplayed(By.cssSelector("#page_options"))) {
+        } else if (widgets.getPagesHelper().isElementDesplayed(By.xpath("//h5[text()='Primary Navigation']"))){
+            widgets.getPagesHelper().openSettingPages(1);
+        } else if (widgets.getPagesHelper().isElementDesplayed(By.cssSelector("#Pages"))) {
             widgets.getPagesHelper().click(By.cssSelector("#pages"));
-        } else if (nameTest == "addBlog") {
-            widgets.getPagesHelper().gotoFrame(By.xpath("//iframe[@class='block-iframe']"));
-            if (!widgets.getPagesHelper().isElementPresent(By.cssSelector(".sb5-empty-text"))) {
-                try {
-                    widgets.getPagesHelper().stopFrame();
-                    widgets.getPagesHelper().click(By.cssSelector("#pages"));
-                    widgets.getPagesHelper().openSettingPages(1);
-                } catch (NoSuchElementException ex) {
-                    widgets.getPagesHelper().addPages("NEW PAGE", 1);
-                }
-            } else {
-                widgets.getPagesHelper().stopFrame();
-            }
-        } else {
-            widgets.getPagesHelper().gotoFrame(By.xpath("//iframe[@class='block-iframe']"));
-            if (!widgets.getPagesHelper().isElementPresent(By.xpath("//div[@class='blog-block row']"))) {
-                widgets.getPagesHelper().stopFrame();
-                widgets.getPagesHelper().click(By.cssSelector("#pages"));
-                widgets.getPagesHelper().openSettingPages(1);
-            } else {
-                widgets.getPagesHelper().stopFrame();
-            }
+            widgets.getPagesHelper().openSettingPages(1);
         }
-    }
+    }*/
 
     @AfterMethod()
     public void restartTest() throws Exception {
