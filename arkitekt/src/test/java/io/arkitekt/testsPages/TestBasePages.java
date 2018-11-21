0435c6bd57c453b46bde40f6d94570ce82927455
BorysovAndriu <36803019+BorysovAndriu@users.noreply.github.com>
2018-11-21 19:09:53 +0200

    Fixe Pages test

--- arkitekt/src/test/java/io/arkitekt/testsPages/TestBasePages.java
+++ arkitekt/src/test/java/io/arkitekt/testsPages/TestBasePages.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.*;
 
+import java.lang.reflect.Method;
+
 public class TestBasePages {
 
     protected static final AppManagerPages pages = new AppManagerPages(BrowserType.CHROME);
@@ -16,20 +18,21 @@ public class TestBasePages {
         pages.driver.manage().addCookie(pages.getPagesHelper().readeCookies());
         pages.getPagesHelper().click(By.xpath("//a[@class='btn btn-link']"));
         /**
-        pages.getPagesHelper().initListSite(true);
-        int before = pages.getPagesHelper().getCountSite();
-        if (before == 1) {
-            pages.getPagesHelper().initListSite(false);
-        } else {
-            pages.getPagesHelper().openFirstSite();
-        }
-        */
+         pages.getPagesHelper().initListSite(true);
+         int before = pages.getPagesHelper().getCountSite();
+         if (before == 1) {
+         pages.getPagesHelper().initListSite(false);
+         } else {
+         pages.getPagesHelper().openFirstSite();
+         }
+         */
         pages.getPagesHelper().click(By.cssSelector("#pages"));
     }
 
-    @AfterMethod()
-    public void restartTest() throws Exception {
-        if(! pages.getPagesHelper().isElementDesplayed(By.xpath("//h5[text()='Primary Navigation']"))) {
+    @BeforeMethod
+    public void restart(Method method) throws Exception {
+        if (!pages.getPagesHelper().
+                isElementPresent(By.xpath("//div[@class='left-block-item-inner pages active-menu']"))) {
             tearDown();
             setUpp();
         }
